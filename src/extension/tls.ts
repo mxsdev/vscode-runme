f2eb1c82d1a5947575c90807cbc0b157855117c7
Max Stoumen <max@mxs.dev>
2023-02-03 21:27:01 -0600

    tlscontext class

---
+++ src/extension/tls.ts
@@ -0,0 +1,14 @@
+/**
+ * Generates and stores 
+ */
+export class TLSContext {
+  private constructor(
+
+  ) { }
+
+  async create(): Promise<TLSContext> {
+    
+    
+    return new TLSContext()
+  }
+}
